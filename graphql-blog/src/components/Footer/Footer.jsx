973d639f17dc1cd428b0476f97cb3f20adcae437
simo994q <simonkn1@hotmail.com>
2023-10-04 10:24:16 +0200

    footer

--- graphql-blog/src/components/Footer/Footer.jsx
+++ graphql-blog/src/components/Footer/Footer.jsx
@@ -1,9 +1,11 @@
-
+import style from './Footer.module.scss'
 
 export const Footer = () => {
     return (
         <>
-            footer
+            <div className={style.footerContainer}>
+                <p>Lavet af simon</p>
+            </div>
         </>
     )
 }
