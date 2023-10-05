856ffdf6304d194653095865939a1a9645a4bde8
simo994q <simonkn1@hotmail.com>
2023-10-05 15:38:25 +0200

    search

--- graphql-blog/src/components/AllPosts/AllPosts.jsx
+++ graphql-blog/src/components/AllPosts/AllPosts.jsx
@@ -3,28 +3,29 @@ import { request } from 'graphql-request'
 import { getAllPosts } from '../../queries/getAllPosts'
 import style from './AllPosts.module.scss'
 import dompurify from 'dompurify';
+import { useState, useEffect } from "react";
 
 export const AllPosts = () => {
 
     let token = import.meta.env.VITE_PUBLIC_URL_TOKEN
 
-    const allPosts = useQuery({
+    const {data, isLoading, error} = useQuery({
         queryKey: ['allPosts'],
         queryFn: async () => request(`https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/${token}/master`, getAllPosts)
     })
 
-    if (allPosts.isLoading) console.log('Loading...');
+    if (isLoading) console.log('Loading...');
 
 
-    if (allPosts.error) console.log(allPosts.error.message);
+    if (error) console.log(error.message);
 
-    console.log(allPosts.data);
+    console.log(data);
 
     const setSort = (sort) => {
         console.log(sort);
         switch (sort) {
             case 'newest':
-                allPosts.data.blogPosts.sort((a, b) => {
+                data.blogPosts.sort((a, b) => {
                     const dateA = new Date(a.postDateTime);
                     const dateB = new Date(b.postDateTime);
 
@@ -33,10 +34,10 @@ export const AllPosts = () => {
                     if (dateA < dateB) return 1;
                     return 0;
                 });
-                console.log(allPosts.data);
+                console.log(data);
                 break;
             case 'oldest':
-                allPosts.data.blogPosts.sort((a, b) => {
+                data.blogPosts.sort((a, b) => {
                     const dateA = new Date(a.postDateTime);
                     const dateB = new Date(b.postDateTime);
 
@@ -44,10 +45,10 @@ export const AllPosts = () => {
                     if (dateA > dateB) return 1;
                     return 0;
                 });
-                console.log(allPosts.data);
+                console.log(data);
                 break;
             case 'name':
-                allPosts.data.blogPosts.sort((a, b) => {
+                data.blogPosts.sort((a, b) => {
                     const nameA = a.postTitle.toLowerCase();
                     const nameB = b.postTitle.toLowerCase();
 
@@ -55,10 +56,28 @@ export const AllPosts = () => {
                     if (nameA > nameB) return 1;
                     return 0;
                 });
-                console.log(allPosts.data);
+                console.log(data);
                 break;
         }
     }
+
+    const [inputText, setInputText] = useState('')
+    const [searchData, setSearchData] = useState()
+
+    const search = () => {
+        let clone = data.blogPosts.map((item) => item)
+        let result = clone.filter((item) => item.postTitle.toLowerCase().includes(inputText.toLowerCase()))
+        console.log(result);
+        setSearchData(result)
+    }
+
+    useEffect(() => {
+        if (inputText == '') {
+            setSearchData()
+        } else {
+            search()
+        }
+    }, [inputText])
 
     return (
         <>
@@ -70,8 +89,10 @@ export const AllPosts = () => {
                 <option value="name">Name</option>
             </select>
 
+            <input type="text" placeholder="Search" onChange={(e) => setInputText(e.target.value)} />
+
             <div className={style.allPostsContainer}>
-                {allPosts?.data?.blogPosts.map((item, index) => {
+                {/* {allPosts?.data?.blogPosts.map((item, index) => {
                     return (
                         <div key={index} className={style.postContainer}>
                             <h3 className={style.postTitle}>{item.postTitle}</h3>
@@ -80,7 +101,24 @@ export const AllPosts = () => {
                         </div>
                     )
                 })
-                }
+                } */}
+                {!searchData ? data?.blogPosts?.map((item, index) => {
+                    return (
+                        <div key={index} className={style.postContainer}>
+                            <h3 className={style.postTitle}>{item.postTitle}</h3>
+                            <p className={style.postDate}>{item.postDateTime.slice(0, 10)} {item.postDateTime.slice(11, 16)}</p>
+                            <div className={style.postTextContainer} dangerouslySetInnerHTML={{ __html: dompurify.sanitize(item.postContent.html) }} />
+                        </div>
+                    )
+                }) : searchData.map((item, index) => {
+                    return (
+                        <div key={index} className={style.postContainer}>
+                            <h3 className={style.postTitle}>{item.postTitle}</h3>
+                            <p className={style.postDate}>{item.postDateTime.slice(0, 10)} {item.postDateTime.slice(11, 16)}</p>
+                            <div className={style.postTextContainer} dangerouslySetInnerHTML={{ __html: dompurify.sanitize(item.postContent.html) }} />
+                        </div>
+                    )
+                })}
             </div>
         </>
     )
